58cd1c97399074116fbab50c24a29bb91da78e46
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-08-22 22:10:50 -0300

    refactor: move server error to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,12 @@
+import { ServerError } from '../errors/server-error'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (body: any): HttpResponse => ({
   statusCode: 400,
   body
+})
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
 })
