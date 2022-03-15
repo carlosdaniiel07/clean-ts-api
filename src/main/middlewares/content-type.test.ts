94ccfeafeb7ff9180fc094ed80f65794fc54e4c3
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-03-15 00:21:40 -0300

    test: ensure XML as content-type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -11,4 +11,15 @@ describe('Content Type middleware', () => {
       .get('/test-content-type')
       .expect('content-type', 'application/json')
   })
+
+  it('should return XML as content-type when forced', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml')
+      return res.send()
+    })
+
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', 'application/xml')
+  })
 })
