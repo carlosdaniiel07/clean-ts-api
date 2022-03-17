97f9e5e999907c9511d7b90ffa47c51f2fab0057
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-03-17 00:01:44 -0300

    feat: create GetAccounts usecase and his implementation

---
+++ src/data/usecases/get-accounts/db-get-accounts.ts
@@ -0,0 +1,13 @@
+import { GetAccounts } from '../../../domain/usecases/get-accounts'
+import { GetAccountsRepository } from '../../protocols/get-accounts-repository'
+import { AccountModel } from '../add-account/db-add-account.protocols'
+
+export class DbGetAccounts implements GetAccounts {
+  constructor (
+    private readonly getAccountsRepository: GetAccountsRepository
+  ) {}
+
+  async getAll (): Promise<AccountModel[]> {
+    return await this.getAccountsRepository.getAll()
+  }
+}
