649ff7b2565a810dfa237d663f558c5e828e7960
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-03-16 23:58:50 -0300

    feat: create GetAccountsRepository and his implementation

---
+++ src/data/protocols/get-accounts-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../../domain/models/account'
+
+export interface GetAccountsRepository {
+  getAll: () => Promise<AccountModel[]>
+}
--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,13 +1,21 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { GetAccountsRepository } from '../../../../data/protocols/get-accounts-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, GetAccountsRepository {
   async add (account: AddAccountModel): Promise<AccountModel> {
     const collection = MongoHelper.getCollection('accounts')
     await collection.insertOne(account)
 
     return MongoHelper.mapToModel<AccountModel>(account)
+  }
+
+  async getAll (): Promise<AccountModel[]> {
+    const collection = MongoHelper.getCollection('accounts')
+    const accounts = await collection.find().toArray()
+
+    return MongoHelper.mapToModels<AccountModel>(accounts)
   }
 }
