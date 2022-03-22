86f9529e44a0b61cb8b18c5afe7dea2e8f4c4f3f
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-03-21 21:23:55 -0300

    test: ensure LogMongoRepository creates an error on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,26 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('LogErrorMongo Repository', () => {
+  let collection: Collection
+
+  beforeAll(async () => await MongoHelper.connect(global.__MONGO_URI__))
+
+  afterAll(async () => await MongoHelper.disconnect())
+
+  beforeEach(async () => {
+    collection = await MongoHelper.getCollection('errors')
+    await collection.deleteMany({})
+  })
+
+  test('should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+
+    await sut.logError('any_error_stack')
+
+    const errorsCount = await collection.countDocuments()
+
+    expect(errorsCount).toBe(1)
+  })
+})
