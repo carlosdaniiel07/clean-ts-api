f32214fa323ee62851b5bee4ca5024abf9518462
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-03-25 00:24:40 -0300

    feat: add token generator implementation (JwtAdapter)

---
+++ src/infra/tokenization/jwt-adapter.ts
@@ -0,0 +1,18 @@
+import { sign } from 'jsonwebtoken'
+import { TokenGenerator } from '../../data/protocols/token-generator'
+import { AccountModel } from '../../domain/models/account'
+import config from '../../main/config/env'
+
+export class JwtAdapter implements TokenGenerator {
+  generate (account: AccountModel): string {
+    const { name, email } = account
+    const token = sign({ name, email }, config.JWT_SECRET_KEY, {
+      issuer: 'clean-ts-api',
+      audience: 'clean-ts-app',
+      expiresIn: '6h',
+      subject: email
+    })
+
+    return token
+  }
+}
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   MONGO_DB_CONNECTION_STRING: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-ts-api',
-  PORT: process.env.PORT ?? 3000
+  PORT: process.env.PORT ?? 3000,
+  JWT_SECRET_KEY: 'my-secret-key'
 }
