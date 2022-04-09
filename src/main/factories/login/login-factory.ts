e107f63020081df770b45bd204813cc0e988f71e
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-04-09 14:42:47 -0300

    test: improve login integration tests

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,8 +1,6 @@
-import { GetAccountByEmailRepository } from '../../../data/protocols/db/account/get-account-by-email-repository'
 import { HashComparer } from '../../../data/protocols/cryptography/hash'
 import { LogErrorRepository } from '../../../data/protocols/db/log/log-error-repository'
 import { TokenGenerator } from '../../../data/protocols/cryptography/token-generator'
-import { UpdateAccessTokenRepository } from '../../../data/protocols/db/account/update-access-token-repository'
 import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
 import { Authentication } from '../../../domain/usecases/authentication'
 import { BCryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
@@ -18,11 +16,9 @@ export const makeLoginController = (): Controller => {
   const encrypterSalt = 12
 
   const accountMongoRepository = new AccountMongoRepository()
-  const getAccountByEmailRepository: GetAccountByEmailRepository = accountMongoRepository
-  const updateAccessTokenRepository: UpdateAccessTokenRepository = accountMongoRepository
   const tokenGenerator: TokenGenerator = new JwtAdapter()
   const hashComparer: HashComparer = new BCryptAdapter(encrypterSalt)
-  const authentication: Authentication = new DbAuthentication(getAccountByEmailRepository, updateAccessTokenRepository, tokenGenerator, hashComparer)
+  const authentication: Authentication = new DbAuthentication(accountMongoRepository, accountMongoRepository, tokenGenerator, hashComparer)
   const loginController: Controller = new LoginController(authentication, makeLoginValidation())
   const logErrorRepository: LogErrorRepository = new LogMongoRepository()
 
---
+++ src/main/routes/login-routes.test.ts
@@ -0,0 +1,68 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
+
+describe('POST /signup', () => {
+  beforeAll(async () => await MongoHelper.connect(global.__MONGO_URI__))
+
+  afterAll(async () => await MongoHelper.disconnect())
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('should return an 201 on signup', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Carlos',
+        email: 'carlos@email.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      })
+      .expect(201)
+  })
+})
+
+describe('POST /login', () => {
+  beforeAll(async () => await MongoHelper.connect(global.__MONGO_URI__))
+
+  afterAll(async () => await MongoHelper.disconnect())
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('should return 200 and an access token on login', async () => {
+    await accountCollection.insertOne({
+      name: 'Carlos',
+      email: 'carlos@email.com',
+      password: await hash('password', 12)
+    })
+
+    const { status, body } = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'carlos@email.com',
+        password: 'password'
+      })
+
+    expect(status).toBe(200)
+    expect(body.accessToken).toBeTruthy()
+  })
+
+  test('should return 401 on login', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'carlos@email.com',
+        password: 'password'
+      }).expect(401)
+  })
+})
