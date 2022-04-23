87537deb0c3903005a37726c82bc455d375f7144
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-04-23 20:48:23 -0300

    feat: add answers size validation on add survey controller

--- src/main/factories/controllers/survey/add-survey-controller-validation-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey-controller-validation-factory.spec.ts
@@ -1,3 +1,4 @@
+import { MinimumArraySizeValidation } from '../../../../validation/validators/minimum-array-size-validation'
 import { RequiredFieldValidation } from '../../../../validation/validators/required-field-validation'
 import { ValidationComposite } from '../../../../validation/validators/validation-composite'
 import { makeAddSurveyValidation } from './add-survey-controller-validation-factory'
@@ -13,7 +14,8 @@ describe('AddSurveyValidation factory', () => {
     expect(ValidationComposite).toHaveBeenCalledWith([
       ...requiredFields.map(
         (fieldName) => new RequiredFieldValidation(fieldName)
-      )
+      ),
+      new MinimumArraySizeValidation('answers', 1)
     ])
   })
 })
