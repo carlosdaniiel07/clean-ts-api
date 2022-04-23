02407e1315001affdc0ea7f5bfc82b9444d81be2
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-04-23 19:48:21 -0300

    feat: create db add survey usecase factory

---
+++ src/main/factories/usecases/db-add-survey-factory.ts
@@ -0,0 +1,9 @@
+import { AddSurveyRepository } from '../../../data/protocols/db/survey/add-survey-repository'
+import { DbAddSurvey } from '../../../data/usecases/add-survey/db-add-survey'
+import { AddSurvey } from '../../../domain/usecases/add-survey'
+import { SurveyMongoRepository } from '../../../infra/db/mongodb/survey/survey-mongo-repository'
+
+export const makeDbAddSurvey = (): AddSurvey => {
+  const addSurveyRepository: AddSurveyRepository = new SurveyMongoRepository()
+  return new DbAddSurvey(addSurveyRepository)
+}
