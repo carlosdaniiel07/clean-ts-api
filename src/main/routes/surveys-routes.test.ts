c5a430128882cbffa9e8ca314e8f54bf4c67c7d9
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-04-27 01:25:25 -0300

    test: ensure add survey route returns 200 if valid access token is provided

--- src/main/routes/surveys-routes.test.ts
+++ src/main/routes/surveys-routes.test.ts
@@ -2,9 +2,12 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import config from '../config/env'
 
 describe('POST /surveys', () => {
   let surveyCollection: Collection
+  let accountCollection: Collection
 
   beforeAll(async () => await MongoHelper.connect(global.__MONGO_URI__))
 
@@ -12,12 +15,54 @@ describe('POST /surveys', () => {
 
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
+
     await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
 
-  test('should return an 201 on add survey', async () => {
+  test('should return 401 on add survey without access token', async () => {
     await request(app)
       .post('/api/surveys')
+      .send({
+        question: 'Qual a sua linguagem de programação preferida?',
+        answers: [
+          {
+            image: 'logo_csharp.png',
+            answer: 'C#'
+          },
+          {
+            answer: 'Type Script'
+          }
+        ]
+      })
+      .expect(401)
+  })
+
+  test('should return 201 on add survey with valid access token', async () => {
+    const account = await accountCollection.insertOne({
+      name: 'Carlos',
+      email: 'carlos@email.com',
+      password: 'any_password',
+      role: 'ADMIN'
+    })
+    const accessToken = sign({
+      id: account.insertedId.toString(),
+      name: 'Carlos',
+      email: 'carlos@email.com'
+    }, config.JWT_SECRET_KEY)
+
+    await accountCollection.updateOne({
+      _id: account.insertedId
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+
+    await request(app)
+      .post('/api/surveys')
+      .set('Authorization', `Bearer ${accessToken}`)
       .send({
         question: 'Qual a sua linguagem de programação preferida?',
         answers: [
