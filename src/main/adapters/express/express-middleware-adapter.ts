b6717f32733d905ea533c451d2d0e2a21fa9d012
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-04-27 01:22:45 -0300

    feat: add auth middleware to /surveys routes

---
+++ src/main/adapters/express/express-middleware-adapter.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express'
+import { HttpRequest, Middleware } from '../../../presentation/protocols'
+
+type ExpressMiddleware = (req: Request, res: Response, next: NextFunction) => void
+
+export const adaptMiddleware = (middleware: Middleware): ExpressMiddleware => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpResponse = await middleware.handle(httpRequest)
+    const isSuccess = httpResponse.statusCode === 200
+
+    if (!isSuccess) {
+      return res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+
+    Object.assign(req, httpResponse.body)
+
+    next()
+  }
+}
