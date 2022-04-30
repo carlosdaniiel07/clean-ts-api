406a5fe39bd78a563f43b9802eea32d1080eb56b
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-04-29 23:48:25 -0300

    refactor: improve controllers factories folder structure

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-validation-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { MinimumArraySizeValidation } from '../../../../validation/validators/minimum-array-size-validation'
-import { RequiredFieldValidation } from '../../../../validation/validators/required-field-validation'
-import { ValidationComposite } from '../../../../validation/validators/validation-composite'
+import { MinimumArraySizeValidation } from '../../../../../validation/validators/minimum-array-size-validation'
+import { RequiredFieldValidation } from '../../../../../validation/validators/required-field-validation'
+import { ValidationComposite } from '../../../../../validation/validators/validation-composite'
 import { makeAddSurveyValidation } from './add-survey-controller-validation-factory'
 
-jest.mock('../../../../validation/validators/validation-composite')
+jest.mock('../../../../../validation/validators/validation-composite')
 
 describe('AddSurveyValidation factory', () => {
   test('should call ValidationComposite with all validations', () => {
---
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-validation-factory.ts
@@ -0,0 +1,14 @@
+import { Validation } from '../../../../../presentation/protocols'
+import { MinimumArraySizeValidation } from '../../../../../validation/validators/minimum-array-size-validation'
+import { RequiredFieldValidation } from '../../../../../validation/validators/required-field-validation'
+import { ValidationComposite } from '../../../../../validation/validators/validation-composite'
+
+export const makeAddSurveyValidation = (): Validation => {
+  const requiredFields = ['question', 'answers']
+  return new ValidationComposite([
+    ...requiredFields.map(
+      (fieldName) => new RequiredFieldValidation(fieldName)
+    ),
+    new MinimumArraySizeValidation('answers', 1)
+  ])
+}
