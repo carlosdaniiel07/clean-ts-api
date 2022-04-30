437bac36c22f9ecd887dfcfb2de581b1bdc40d57
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-04-29 22:58:05 -0300

    refactor: use helper method on LoadSurveysController

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,5 +1,5 @@
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
-import { serverError } from '../../../helpers/http-helper'
+import { ok, serverError } from '../../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../../protocols'
 
 export class LoadSurveysController implements Controller {
@@ -8,10 +8,7 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const data = await this.loadSurveys.load()
-      return {
-        statusCode: 200,
-        body: data
-      }
+      return ok(data)
     } catch (err) {
       return serverError(err)
     }
