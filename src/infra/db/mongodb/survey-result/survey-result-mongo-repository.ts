fdbf7b10a74cd8dde8bac1ee0114bf1b24e2db0f
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-05-08 21:33:38 -0300

    feat: add LoadSurveyResultBySurveyRepository data protocol

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,7 +1,10 @@
 import { ObjectId } from 'mongodb'
 import { AddSurveyResultRepository } from '~/data/protocols/db/survey-result/add-survey-result-repository'
 import { CountSurveyResultByAccountAndSurveyRepository } from '~/data/protocols/db/survey-result/count-survey-result-by-account-and-survey'
+import { LoadSurveyResultBySurveyRepository } from '~/data/protocols/db/survey-result/load-survey-result-by-survey-repository'
 import { UpdateSurveyResultRepository } from '~/data/protocols/db/survey-result/update-survey-result-repository'
+import { SurveyAnswerModel, SurveyModel } from '~/domain/models/survey'
+import { SurveyResultModel } from '~/domain/models/survey-result'
 import { SaveSurveyResultParams } from '~/domain/usecases/save-survey-result'
 import { MongoHelper } from '~/infra/db/mongodb/helpers/mongo-helper'
 
@@ -9,7 +12,8 @@ export class SurveyResultMongoRepository
 implements
     CountSurveyResultByAccountAndSurveyRepository,
     AddSurveyResultRepository,
-    UpdateSurveyResultRepository {
+    UpdateSurveyResultRepository,
+    LoadSurveyResultBySurveyRepository {
   async countByAccountAndSurvey (
     accountId: string,
     surveyId: string
@@ -47,5 +51,55 @@ implements
         }
       }
     )
+  }
+
+  async loadBySurvey (surveyId: string): Promise<SurveyResultModel | null> {
+    const survey = MongoHelper.mapToModel<SurveyModel>(
+      await (
+        await MongoHelper.getCollection('surveys')
+      ).findOne({
+        _id: new ObjectId(surveyId)
+      })
+    )
+    const collection = await MongoHelper.getCollection('survey_results')
+    const surveyResults = await collection
+      .aggregate([
+        {
+          $match: {
+            surveyId: new ObjectId(surveyId)
+          }
+        },
+        {
+          $group: {
+            _id: '$answer',
+            count: {
+              $sum: 1
+            }
+          }
+        }
+      ])
+      .toArray()
+    const totalResults = surveyResults
+      ?.map(({ count }) => count as number)
+      .reduce((total, current) => total + current, 0)
+
+    return {
+      surveyId,
+      question: survey.question,
+      date: survey.date,
+      answers: surveyResults?.map((item) => {
+        const { _id: answer, count } = item
+        const { image } = survey.answers.find(
+          (surveyAnswer) => surveyAnswer.answer === answer
+        ) as SurveyAnswerModel
+
+        return {
+          answer,
+          count,
+          image,
+          percent: (count / totalResults) * 100
+        }
+      })
+    }
   }
 }
