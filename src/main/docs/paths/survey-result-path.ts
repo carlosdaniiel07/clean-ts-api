b3d9c4b8549093626be174ab1277877205e972c7
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-05-21 16:08:04 -0300

    feat: update Swagger docs with load survey result controller

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -1,4 +1,45 @@
 export const surveyResultPath = {
+  get: {
+    tags: ['Enquete'],
+    summary: 'Retorna o resultado de uma enquete',
+    description: 'Retorna o resultado de uma enquete',
+    security: [
+      {
+        bearerAuth: []
+      }
+    ],
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string'
+        }
+      }
+    ],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      401: {
+        $ref: '#/components/unauthorized'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
   post: {
     tags: ['Enquete'],
     summary: 'Registra uma nova resposta para uma enquete',
