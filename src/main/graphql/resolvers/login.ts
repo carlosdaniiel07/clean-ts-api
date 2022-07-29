9e2bb007d60aba9b7c499f0f0005e72de3754c02
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-07-28 21:45:30 -0300

    feat: add GraphQL support to login endpoint

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,11 +1,10 @@
+import { adaptResolver } from '~/main/adapters/apollo/apollo-server-resolver-adapter'
+import { makeLoginController } from '~/main/factories/controllers/login/login-controller-factory'
+import { LoginController } from '~/presentation/controllers/login/login/login-controller'
+
 export default {
   Query: {
-    login () {
-      return {
-        accessToken: 'accessToken',
-        name: 'name',
-        email: 'email'
-      }
-    }
+    login: async (_: any, args: LoginController.Request) =>
+      await adaptResolver(makeLoginController(), args)
   }
 }
