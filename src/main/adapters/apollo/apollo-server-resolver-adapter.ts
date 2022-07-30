b27ef62f5acb0de7252a54ca0c05e6be0e910b27
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2022-07-30 00:07:31 -0300

    refactor: improvements on ApolloServerResolverAdapter

--- src/main/adapters/apollo/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo/apollo-server-resolver-adapter.ts
@@ -1,9 +1,28 @@
+import {
+  ApolloError,
+  AuthenticationError,
+  ForbiddenError,
+  UserInputError
+} from 'apollo-server-express'
 import { Controller } from '~/presentation/protocols'
 
 export const adaptResolver = async (
   controller: Controller,
   args: any
 ): Promise<any> => {
-  const httpResponse = await controller.handle(args)
-  return httpResponse.body
+  const { statusCode, body } = await controller.handle(args)
+
+  switch (statusCode) {
+    case 200:
+    case 204:
+      return body
+    case 400:
+      throw new UserInputError(body.message)
+    case 401:
+      throw new AuthenticationError(body.message)
+    case 403:
+      throw new ForbiddenError(body.message)
+    default:
+      throw new ApolloError(body.message)
+  }
 }
